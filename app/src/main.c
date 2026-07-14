ba8db8a2db35d8eac70e5ab6595420a6ddb7c0f7
Филиппов Егор <egor207581@gmail.com>
2026-07-14 08:23:44 +0300

    feat(app): добавлен цикл getopt и структура по умолчанию

--- app/src/main.c
+++ app/src/main.c
@@ -9,6 +9,17 @@
 #include "hexdump_lib.h"
 
 int main(int argc, char *argv[]) {
+	DumpOptions opts = {0, 0, 1, 16, NULL};
+    	char *filepath = NULL;
+    	char *dirpath = NULL;
+    	int opt;
+
+    	while ((opt = getopt(argc, argv, "i:o:l:g:n:d:f:")) != -1) {
+        	switch (opt) {
+            		// Здесь будет обработка
+        	}
+    	}
+
 	return 0;
 }
 
