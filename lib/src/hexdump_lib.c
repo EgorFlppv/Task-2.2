f5c79d9e9387fccec4f665d1b9f8adee8500d262
Филиппов Егор <egor207581@gmail.com>
2026-07-14 08:17:29 +0300

    feat(lib): создан каркас hexdump_lib.c

---
+++ lib/src/hexdump_lib.c
@@ -0,0 +1,13 @@
+/*
+ * Описание файла: Реализация hex-дампа.
+ * ФИО: Филиппов Егор Ильич
+ * Группа: МК-102
+ */
+#include "hexdump_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int dump_file(const char *filepath, const DumpOptions *opts) {
+	return 0;
+}
