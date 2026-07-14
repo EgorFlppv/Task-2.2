44368ca84fe2509d07132ab2d448c69e07ec530a
Филиппов Егор <egor207581@gmail.com>
2026-07-14 08:38:58 +0300

    feat(lib): реализован цикл чтения с учетом ограничений опций

--- lib/src/hexdump_lib.c
+++ lib/src/hexdump_lib.c
@@ -18,7 +18,28 @@ int dump_file(const char *filepath, const DumpOptions *opts) {
     	unsigned char @id4557802 (*buffer) = malloc(buffer_size);
     	if (!buffer) { fclose(f); return -1; }
 
-    	// Переменные для цикла
+    	size_t current_offset = opts->offset;
+    	size_t line_idx = 0;
+    	size_t total_read = 0;
+
+    	while (1) {
+        	size_t to_read = buffer_size;
+        	if (opts->size > 0 && (opts->size - total_read) < buffer_size) {
+            		to_read = opts->size - total_read;
+        	}
+        	if (to_read == 0) break;
+
+        	size_t bytes = fread(buffer, 1, to_read, f);
+        	if (bytes == 0) break;
+
+        	// Здесь будет вывод
+        
+        	current_offset += bytes;
+        	total_read += bytes;
+        	line_idx++;
+        	if (bytes < buffer_size) break; // Конец файла
+    	}
+
     	free(buffer);
     	fclose(f);
     	return 0;
